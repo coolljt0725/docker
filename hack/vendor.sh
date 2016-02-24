d19fd9b2f218745b6026a3e961ace696332be480
Tonis Tiigi <tonistiigi@gmail.com>
2016-02-24 06:53:29 -0800

    Vendor containerd, specs, runc
    Also contains temporary libnetwork patch.
    
    Signed-off-by: Tonis Tiigi <tonistiigi@gmail.com>
    Signed-off-by: Kenfe-Mickael Laventure <mickael.laventure@gmail.com>

--- hack/vendor.sh
+++ hack/vendor.sh
@@ -29,7 +29,8 @@ clone git github.com/RackSec/srslog 6eb773f331e46fbba8eecb8e794e635e75fc04de
 clone git github.com/imdario/mergo 0.2.1
 
 #get libnetwork packages
-clone git github.com/docker/libnetwork v0.7.0-dev.3
+#temp fork for pr
+clone git github.com/docker/libnetwork v0.7.0-dev.3-hookfix https://github.com/tonistiigi/libnetwork.git
 clone git github.com/armon/go-metrics eb0af217e5e9747e41dd5303755356b62d28e3ec
 clone git github.com/hashicorp/go-msgpack 71c2886f5a673a35f909803f38ece5810165097b
 clone git github.com/hashicorp/memberlist 9a1e242e454d2443df330bdd51a436d5a9058fc4
@@ -60,12 +61,13 @@ clone git github.com/docker/go v1.5.1-1-1-gbaf439e
 clone git github.com/agl/ed25519 d2b94fd789ea21d12fac1a4443dd3a3f79cda72c
 
 clone git github.com/opencontainers/runc 2c3115481ee1782ad687a9e0b4834f89533c2acf # libcontainer
+clone git github.com/opencontainers/specs 25cbfc427ba6154016f33c7ed1b8ed43b8b7b7ed # specs
 clone git github.com/seccomp/libseccomp-golang 1b506fc7c24eec5a3693cdcbed40d9c226cfc6a1
 # libcontainer deps (see src/github.com/opencontainers/runc/Godeps/Godeps.json)
 clone git github.com/coreos/go-systemd v4
 clone git github.com/godbus/dbus v3
 clone git github.com/syndtr/gocapability 2c00daeb6c3b45114c80ac44119e7b8801fdd852
-clone git github.com/golang/protobuf f7137ae6b19afbfd61a94b746fda3b3fe0491874
+clone git github.com/golang/protobuf 68415e7123da32b07eab49c96d2c4d6158360e9b
 
 # gelf logging driver deps
 clone git github.com/Graylog2/go-gelf 6c62a85f1d47a67f2a5144c0e745b325889a8120
@@ -78,5 +80,8 @@ clone git github.com/tinylib/msgp 75ee40d2601edf122ef667e2a07d600d4c44490c
 # awslogs deps
 clone git github.com/aws/aws-sdk-go v0.9.9
 clone git github.com/vaughan0/go-ini a98ad7ee00ec53921f08832bc06ecf7fd600e6a1
+
+clone git google.golang.org/grpc a22b6611561e9f0a3e0919690dd2caf48f14c517 https://github.com/grpc/grpc-go.git
+clone git github.com/docker/containerd 55c69b240f837fc7a4b417ce14fe560460d426dd
 
 clean
